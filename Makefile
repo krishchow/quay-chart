f0b3456283a9ae871ed98c11ba3a52c3a2937cf2
Krish Chowdhary <krish@redhat.com>
2021-03-02 17:04:44 -0500

    minor cleaning, reduce wait

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ build:
 	helm package quay --destination .deploy
 
 upload:
-	cr upload -o krishchow -r quay-chart -p .deploy --token $CH_TOKEN
+	cr upload -o krishchow -r quay-chart -p .deploy --token ${CH_TOKEN}
--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ kubectl get secret $s3secret -n $namespace
 while [[ $? -eq 1 ]] || [[ $(kubectl get secret $s3secret -n $namespace | tail -n 1 | awk '{print $3}') -ne 3 ]]
 do
 echo 'Waiting for S3 Bucket'
-sleep 5
+sleep 1
 kubectl get secret $s3secret -n $namespace > /dev/null 2>&1
 done
 
@@ -14,7 +14,7 @@ kubectl get secret $redissecret -n $namespace
 while [[ $? -eq 1 ]] || [[ $(kubectl get secret $redissecret -n $namespace | tail -n 1 | awk '{print $3}') -ne 2 ]]
 do
 echo 'Waiting for Redis'
-sleep 5
+sleep 1
 kubectl get secret $redissecret -n $namespace > /dev/null 2>&1
 done
 
@@ -24,7 +24,7 @@ kubectl get secret $pgsecret -n $namespace
 while [[ $? -eq 1 ]] || [[ $(kubectl get secret $pgsecret -n $namespace | tail -n 1 | awk '{print $3}') -ne 6 ]]
 do
 echo 'Waiting for Postgres'
-sleep 5
+sleep 1
 kubectl get secret $pgsecret -n $namespace > /dev/null 2>&1
 done
 
